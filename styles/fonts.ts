409be8c31b81af927d00fdfb2db5227d5146b43b
VladislavSMI <69860591+VladislavSMI@users.noreply.github.com>
2024-11-13 17:45:37 +0100

    style: SK-3 update fonts

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -1,7 +1,7 @@
-import { Inter, Lusitana } from "next/font/google";
+import { Montserrat, Roboto } from 'next/font/google';
 
-export const inter = Inter({ subsets: ["latin"] });
-export const lusitana = Lusitana({
-  subsets: ["latin"],
-  weight: ["400", "700"],
+export const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['400', '700'],
 });
+export const roboto = Roboto({ subsets: ['latin'], weight: ['400', '700'] });

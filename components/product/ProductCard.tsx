fd79a32fddfcbf58dcbf41591cf10c67d603d7fb
VladislavSMI <69860591+VladislavSMI@users.noreply.github.com>
2024-11-13 17:42:45 +0100

    feat: SK-3 update ProductCard component

--- components/product/ProductCard.tsx
+++ components/product/ProductCard.tsx
@@ -1,25 +1,27 @@
-import React, { FC, FunctionComponent } from 'react';
+import React from 'react';
 import Image from 'next/image';
 import { Product } from '@/lib/types';
 
-export const ProductCard: FunctionComponent<{ product: Product }> = ({
-  product: { image_url, product_name, price }
-}) => {
+export function ProductCard({
+  product: { imageUrl, productName, price },
+}: {
+  product: Product;
+}) {
   return (
     <div className="h-80 w-64 rounded-lg border border-transparent bg-black p-4 text-white shadow-lg transition-all duration-300 hover:border-gray-500 hover:shadow-2xl">
       <div className="relative h-48 w-full">
         <Image
-          src={image_url}
-          alt={product_name}
+          src={imageUrl}
+          alt={productName}
           fill
           className="rounded-lg transition-transform duration-500 hover:scale-105 dark:invert"
         />
       </div>
 
       <div className="mt-4 text-center">
-        <h3 className="text-lg font-semibold">{product_name}</h3>
+        <h3 className="text-lg font-semibold">{productName}</h3>
         <p className="text-gray-400">${price}</p>
       </div>
     </div>
   );
-};
+}

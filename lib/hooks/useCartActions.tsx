54fe8d7e639c46c8217d65518d98920b77bded19
VladislavSMI <69860591+VladislavSMI@users.noreply.github.com>
2024-12-06 11:58:19 +0100

    feat: SK-8 create useCartActions hook

---
+++ lib/hooks/useCartActions.tsx
@@ -0,0 +1,64 @@
+import { useState, useTransition } from 'react';
+import { useTranslations } from 'next-intl';
+import { addItem } from '@/lib/actions/cartActions';
+import { resetMessages } from '@/lib/utils/resetMessages';
+import { CartSelection, ValidatedCartSelection } from '../types';
+import { validateCartSelection } from '../utils/validateCartSlection';
+import { handleApiResponse } from '../utils/handleApiResponse';
+
+export function useCartActions() {
+  const [isPending, startTransition] = useTransition();
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [errorFields, setErrorFields] = useState<Record<string, string> | null>(
+    null,
+  );
+
+  const [cartSelection, setCartSelection] = useState<CartSelection>({
+    productId: null,
+    sizeId: null,
+    quantity: 1,
+  });
+
+  const t = useTranslations();
+
+  const updateCartSelection = (updates: Partial<CartSelection>) => {
+    setCartSelection((prev) => ({ ...prev, ...updates }));
+  };
+
+  const addToCart = (): void => {
+    resetMessages(setSuccessMessage, setErrorMessage, setErrorFields);
+
+    if (
+      !validateCartSelection({
+        cartSelection,
+        translate: t,
+        setErrorMessage,
+      })
+    ) {
+      return;
+    }
+
+    startTransition(async () => {
+      const response = await addItem(cartSelection as ValidatedCartSelection);
+
+      handleApiResponse({
+        response,
+        translate: t,
+        setSuccessMessage,
+        setErrorMessage,
+        setErrorFields,
+      });
+    });
+  };
+
+  return {
+    isPending,
+    cartSelection,
+    successMessage,
+    errorMessage,
+    errorFields,
+    updateCartSelection,
+    addToCart,
+  };
+}

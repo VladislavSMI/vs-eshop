5cf533453d5bc7f08a291b44d0c0e8f895bde8d1
VladislavSMI <69860591+VladislavSMI@users.noreply.github.com>
2024-12-06 11:52:19 +0100

    feat: SK-4 create Carousel ui component

---
+++ components/ui/Carousel.tsx
@@ -0,0 +1,21 @@
+import { Product } from '@/lib/types';
+import { ProductCardWrapper } from '@/components/product/ProductCard/ProductCardWrapper';
+
+export function Carousel({ products }: { products: Product[] }) {
+  if (!products?.length) return null;
+
+  return (
+    <div className="w-full overflow-x-auto pb-6 pt-1">
+      <ul className="flex animate-carousel gap-4">
+        {products.map((product) => (
+          <li
+            key={product.productId}
+            className="relative aspect-square h-[45vh] max-h-[350px] w-2/3 max-w-[300px] flex-none md:w-1/3"
+          >
+            <ProductCardWrapper product={product} useTilt={false} useLink />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}

9aaa2b6cd5048f7099398afa5d1e2bbcc425a75f
VladislavSMI <69860591+VladislavSMI@users.noreply.github.com>
2024-12-06 11:52:03 +0100

    feat: SK-8 update Navbar

--- components/layout/navbar/Navbar.tsx
+++ components/layout/navbar/Navbar.tsx
@@ -1,20 +1,21 @@
-'use client';
-
-import { useTranslations } from 'next-intl';
 import React, { Suspense } from 'react';
-import { Bars3Icon, ShoppingCartIcon } from '@heroicons/react/24/outline';
-import { Link } from '@/i18n/routing';
+import { getTranslations } from 'next-intl/server';
+import { Bars3Icon, HomeIcon } from '@heroicons/react/24/outline';
 
+import { Link } from '@/i18n/routing';
 import Search from '@/components/search/Search';
 import { SearchSkeleton } from '@/components/search/SearchSkeleton';
 import { Dropdown } from '@/components/ui/DropDown';
-import { Category } from '@/lib/types';
 import LanguageSwitcher from '@/components/layout/language-switcher/LanguageSwitcher';
 import { LanguageSwitcherSkeleton } from '@/components/layout/language-switcher/LanguageSwitcherSkeleton';
 import { Logo } from '@/components/icons/Logo';
+import CartButton from '@/components/layout/cart-button/CartButton';
+import { getAllCategoriesUseCase } from '@/use-cases/categories';
+import { CartButtonSkeleton } from '@/components/layout/cart-button/CartButtonSkeleton';
 
-export default function Navbar({ categories }: { categories: Category[] }) {
-  const t = useTranslations('navigation');
+export default async function Navbar() {
+  const t = await getTranslations('navigation');
+  const categories = await getAllCategoriesUseCase();
 
   return (
     <nav className="sticky top-0 z-50 flex w-full items-center justify-between bg-base-100 p-3 shadow-md">
@@ -25,6 +26,15 @@ export default function Navbar({ categories }: { categories: Category[] }) {
           contentClassName="w-52 rounded-box"
         >
           <ul className="menu p-2">
+            <li className="mb-4 border-b-[0.5px] border-primary md:hidden">
+              <Link
+                href="/"
+                className="flex items-center space-x-2 py-2 hover:border-primary hover:text-primary"
+              >
+                <HomeIcon className="h-6 w-6 animate-breathing" />
+                <span className="text-lg font-semibold">{t('home')}</span>
+              </Link>
+            </li>
             {categories.map((category) => (
               <li key={category.categoryId}>
                 <Link
@@ -61,28 +71,11 @@ export default function Navbar({ categories }: { categories: Category[] }) {
         <Suspense fallback={<LanguageSwitcherSkeleton />}>
           <LanguageSwitcher />
         </Suspense>
-        <Dropdown
-          icon={
-            <div className="indicator">
-              <span className="badge indicator-item">8</span>
-              <ShoppingCartIcon className="h-7 w-7" />
-            </div>
-          }
-          position="right"
-          contentClassName="w-52 rounded-lg"
-        >
-          <div className="card text-center">
-            <div className="card-body p-5">
-              <span className="text-lg font-bold">8 Items</span>
-              <span className="text-info">Subtotal: $999</span>
-              <div className="card-actions">
-                <Link href="/" className="btn btn-block">
-                  {t('cart')}
-                </Link>
-              </div>
-            </div>
-          </div>
-        </Dropdown>
+        <div>
+          <Suspense fallback={<CartButtonSkeleton />}>
+            <CartButton />
+          </Suspense>
+        </div>
       </div>
     </nav>
   );
--- components/layout/navbar/NavbarWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { getAllCategories } from '@/data/repository/ProductRepository';
-import Navbar from './Navbar';
-
-export default async function NavbarWrapper() {
-  const categories = await getAllCategories();
-
-  return <Navbar categories={categories} />;
-}

fcbd195472ff9ed3fc3c180d0ea08a7165ad833e
VladislavSMI <69860591+VladislavSMI@users.noreply.github.com>
2025-05-26 22:11:04 +0200

    style: SK-19 update product card component

--- components/product/ProductCard/ProductCardWrapper.tsx
+++ components/product/ProductCard/ProductCardWrapper.tsx
@@ -19,15 +19,13 @@ interface ProductCardWrapperProps {
  * - **LinkWrapper Behavior:**
  *   - We have two LinkWrapper components:
  *   - 1. Outer LinkWrapper wraps the entire ProductCard when useLink is true.
- *   - 2. Inner LinkWrapper (inside ProductCard) wraps only the ProductCardInfo component.
+ *   - 2. Inner LinkWrapper (inside ProductCard) wraps only the ProductCardInfo component(product name and price).
  *   - Only one LinkWrapper should be active at a time to avoid nested links issues:
  *      - When useLink is true, the outer LinkWrapper is active, and the inner LinkWrapper is disabled.
  *      - When useLink is false, the outer LinkWrapper is disabled, and the inner LinkWrapper is active.
  *   - This is achieved by passing !useLink to the inner LinkWrapper's useLink prop.
- *
- * - **Image Behavior:**
- *   - Uses `dark:invert` to invert colors in dark mode.
- *   - In production, images should have transparent backgrounds to prevent color distortion.
+ *  - Why are we doing this? For example, in normal case we want the entire image be clickable, but in some cases we want only the product name and price to be clickable
+ *    e.g. 3D model where user can click on the image to rotate the model.
  */
 
 export const ProductCardWrapper = ({
@@ -55,7 +53,7 @@ export const ProductCardWrapper = ({
       >
         {imageComponent || (
           <Image
-            className="rounded-lg border-none object-contain p-5 transition-transform duration-500 hover:scale-[1.1] dark:invert"
+            className="rounded-lg border-none object-contain p-5 transition-transform duration-500 hover:scale-[1.1]"
             src={getPublicUrl(mainImageUrl)}
             alt={productName}
             fill

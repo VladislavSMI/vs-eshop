23e4d15edb836ad0d82a7f33a31c110f67aa4f77
VladislavSMI <69860591+VladislavSMI@users.noreply.github.com>
2025-06-05 19:46:06 +0200

    chore: SK-23 extract category functions from ProductRepository

--- use-cases/categories.ts
+++ use-cases/categories.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { getAllCategories } from '@/data/repository/ProductRepository';
+import { getAllCategories } from '@/data/repository/CategoryRepository';
 import { Category } from '@/lib/types';
 
 export async function getAllCategoriesUseCase(): Promise<Category[]> {

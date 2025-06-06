4688bd0e894c31ee56fa83e53fbb1ef66ad7ed39
VladislavSMI <69860591+VladislavSMI@users.noreply.github.com>
2025-06-06 09:55:13 +0200

    fix: SK-23 update seed user API endpoint to POST request and add verification

--- app/api/seed/route.ts
+++ app/api/seed/route.ts
@@ -1,14 +1,41 @@
+import { NextRequest, NextResponse } from 'next/server';
 import { seedUsers } from '@/data/seeds/seedUsers';
 import { log } from '@/lib/logging/log';
+import { verifyMethodAndToken } from '@/lib/utils/apiUtils/verifyMethodAndToken';
 
-export async function GET() {
+export async function POST(req: NextRequest) {
+  log.info('Get seed users request received');
+
+  const { method } = req;
+  const { searchParams, pathname } = req.nextUrl;
+  const token = searchParams.get('token') as string;
+
+  const isTokenAndMethodVerified = verifyMethodAndToken({
+    token,
+    tokenKey: 'seed',
+    method,
+    expectedMethod: 'POST',
+    apiEndpoint: pathname,
+  });
+
+  if (!isTokenAndMethodVerified) {
+    return NextResponse.json(
+      { error: 'Invalid token or method' },
+      { status: 403 },
+    );
+  }
+
   try {
     await seedUsers();
     log.info('Users seeded successfully');
 
-    return Response.json({ message: 'Users seeded successfully' });
+    return NextResponse.json({ message: 'Users seeded successfully' });
   } catch (error) {
     log.error({ error }, 'Failed to seed users');
-    return Response.json({ error: 'Failed to seed users' }, { status: 500 });
+
+    return NextResponse.json(
+      { error: 'Failed to seed users' },
+      { status: 500 },
+    );
   }
 }
